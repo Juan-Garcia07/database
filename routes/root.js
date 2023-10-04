0953f2e95f6fd9108d2edb13c0dd9cdadf5fa39e
Jose Juan Garcia Flores <isc20350534@gmail.com>
2023-10-03 21:45:49 -0600

    fifth commit

---
+++ routes/root.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const usersRoutes = require('./users')
+
+router.use('/users', usersRoutes)
+
+module.exports = router
+
+// http://localhost:3000/api/v1/users
