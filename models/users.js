146ac5b0c55024f37bd1192730415c455d9df13b
Jose Juan Garcia Flores <isc20350534@gmail.com>
2023-10-16 20:38:15 -0600

    updated

--- models/users.js
+++ models/users.js
@@ -13,6 +13,40 @@ const usersModel = {
                        WHERE
                             id=?
                 `,
+                getByUsername:`
+                   SELECT
+                        *
+                   FROM
+                          Users
+                  WHERE
+                       username = ?
+
+                       `,
+                       getByEmail:`
+                   SELECT
+                        *
+                   FROM
+                          Users
+                  WHERE
+                       email = ?
+
+                       `,
+                       addRow:`
+                       INSERT INTO
+                       Users(
+                         username,
+                         password,
+                         email,
+                         name,
+                         lastname,
+                         phonenumber,
+                         role_id,
+                         is_active
+                       )VALUES(
+                         ?,?,?,?,?,?,?,?
+                       )
+                       `,
 }
+
 
 module.exports = usersModel;
